89c764308117276d256dba420c2ee8784f5ef123
Jon <jonantolin@gmail.com>
2019-10-28 17:54:20 +0100

    menu css activo

--- app/components/menu/menu-component.js
+++ app/components/menu/menu-component.js
@@ -1,11 +1,44 @@
 angular.module('phonecatApp').component('menuNav', {
 
     templateUrl: './components/menu/menu-template.html',
-    controller: [ function menuNav(){
+    controller: ['$scope', function menuNav($scope){
 
         
 
         var self = this;
+
+        $scope.enlaces = [
+            {
+             "nombre": "Inicio",
+             "ruta": "#!/phones",
+             "activo": true
+            },
+            {
+             "nombre": "Comparador",
+             "ruta": "#!/comparador",
+             "activo": false
+            },
+             {
+             "nombre": "Carrito",
+             "ruta": "#!/carrito",
+             "activo": false
+            },
+             {
+             "nombre": "Nuevo",
+             "ruta": "#!/nuevo",
+             "activo": false
+            }
+
+        ];
+
+        this.cambiarActivo = function(enlace){
+
+            $scope.enlaces.forEach(element => {
+                element.activo = false;
+            });
+            enlace.activo = true;
+
+        }
 
        
     }]
